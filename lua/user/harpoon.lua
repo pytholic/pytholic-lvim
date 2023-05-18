e34e3ad2983c71a116e34c5c5d0a7e6ba8a386c5
pytholic <rajahaseeb147@gmail.com>
2023-05-18 12:53:41 +0900

    harpoon remap

--- lua/user/harpoon.lua
+++ lua/user/harpoon.lua
@@ -3,9 +3,15 @@ local ui = require("harpoon.ui")
 
 lvim.keys.normal_mode["<leader>a"] = mark.add_file
 lvim.keys.normal_mode["<C-h>"] = ui.toggle_quick_menu
-lvim.keys.normal_mode["<C-[>"] = function()
+lvim.keys.normal_mode["<C-p>"] = function()
 	ui.nav_file(1)
 end
+lvim.keys.normal_mode["<C-[>"] = function()
+	ui.nav_file(2)
+end
 lvim.keys.normal_mode["<C-]>"] = function()
+	ui.nav_file(2)
+end
+lvim.keys.normal_mode["<C->"] = function()
 	ui.nav_file(2)
 end

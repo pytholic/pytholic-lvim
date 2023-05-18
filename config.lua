4b7125f36802d3e9a03e1c3897a07dac85f4f005
pytholic <rajahaseeb147@gmail.com>
2023-05-18 15:57:14 +0900

    clang cpplint

--- config.lua
+++ config.lua
@@ -7,6 +7,7 @@ local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
 	{ name = "black" },
 	{ name = "stylua" },
+	{ name = "clang_format" },
 	{
 		name = "prettier",
 		---@usage arguments to pass to the formatter
@@ -27,6 +28,30 @@ linters.setup({
 	},
 	{ name = "cpplint", filetypes = { "cpp" } },
 })
+
+local clangd_flags = {
+	"--all-scopes-completion",
+	"--suggest-missing-includes",
+	"--background-index",
+	"--pch-storage=disk",
+	"--cross-file-rename",
+	"--log=info",
+	"--completion-style=detailed",
+	"--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+	"--clang-tidy",
+	"--offset-encoding=utf-16", --temporary fix for null-ls
+	"--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+	"--fallback-style=Google",
+	"--header-insertion=never",
+	"--query-driver=<list-of-white-listed-complers>",
+}
+
+local clangd_bin = "clangd"
+
+local opts = {
+	cmd = { clangd_bin, unpack(clangd_flags) },
+}
+require("lvim.lsp.manager").setup("clangd", opts)
 
 -- Additional plugins
 lvim.plugins = {

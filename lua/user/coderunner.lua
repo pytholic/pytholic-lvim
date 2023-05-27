44047b97dc5d80ca77001b7016e886beb2daf832
pytholic <rajahaseeb147@gmail.com>
2023-05-27 16:47:58 +0900

    coderunner clang

--- lua/user/coderunner.lua
+++ lua/user/coderunner.lua
@@ -1,18 +1,23 @@
 require("code_runner").setup({
-    mode = "toggleterm",
-    -- put here the commands by filetype
-    filetype = {
-        java = {
-            "cd $dir &&",
-            "javac $fileName &&",
-            "java $fileNameWithoutExt",
-        },
-        python = "python3 -u",
-        typescript = "deno run",
-        rust = {
-            "cd $dir &&",
-            "rustc $fileName &&",
-            "$dir/$fileNameWithoutExt",
-        },
-    },
+	mode = "toggleterm",
+	-- put here the commands by filetype
+	filetype = {
+		java = {
+			"cd $dir &&",
+			"javac $fileName &&",
+			"java $fileNameWithoutExt",
+		},
+		python = "python3 -u",
+		typescript = "deno run",
+		rust = {
+			"cd $dir &&",
+			"rustc $fileName &&",
+			"$dir/$fileNameWithoutExt",
+		},
+		cpp = {
+			"cd $dir &&",
+			"clang++ -Weverything $file -o $fileNameWithoutExt &&",
+			"$dir/$fileNameWithoutExt",
+		},
+	},
 })
